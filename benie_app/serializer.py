1072f1d40389b4115c380220530a9e4a863f696c
benie254 <davinci.monalissa@gmail.com>
2022-09-15 20:58:14 +0300

    create myInfo model & update profile model & serializer

--- benie_app/serializer.py
+++ benie_app/serializer.py
@@ -4,4 +4,4 @@ from benie_app.models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile 
-        fields = ('id','first_name','last_name','username','email','phone_number','resume','bio','intro','about_me','profile_photo')
+        fields = ('id','first_name','last_name','username','email','phone_number',)
