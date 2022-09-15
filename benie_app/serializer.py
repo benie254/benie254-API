673a67c29306d0bfe573196343220bda173fa753
benie254 <davinci.monalissa@gmail.com>
2022-09-15 21:00:11 +0300

    create my info serializer

--- benie_app/serializer.py
+++ benie_app/serializer.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from benie_app.models import Profile
+from benie_app.models import MyInfo, Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile 
         fields = ('id','first_name','last_name','username','email','phone_number',)
+
+
+class MyInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MyInfo
+        fields = ('resume','bio','intro','about_me','profile_photo',)
